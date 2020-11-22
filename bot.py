118b48a3d3bdb74fb335a7c10a9c364da4e33109
rishi432 <39430301+rishi432@users.noreply.github.com>
2020-11-22 15:19:44 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,13 +2,13 @@ import telebot
 import redis
 r = redis.Redis('localhost')
 from tempmail import TempMail
-token = '1461005584:AAEiZtvhP6rmdBuTvnogO7pKdObsaks-0VA'
+token = '1461005584:AAEiZtvhP6rmdBuTvnogO7pKdObsaks-0VA' #put your token here
 bot = telebot.TeleBot(token)
 user = bot.get_me().username
 @bot.message_handler(commands=['start'])
 def start(m):
   # start text
-  text = 'Hi.\nWelcome to {}.\nCommands: \n/newmail: For making a new Email.\n/mails: For reading new emails.'.format(user)
+  text = '📧Hi.\nWelcome to {}.\nCommands: \n/newmail: For making a new Email.\n/mails: For reading new emails.\nDeveloped by @Electrovirus'.format(user)
   bot.send_message(m.chat.id, text)
 @bot.message_handler(commands=['newmail'])
 def newmail(m):
@@ -16,28 +16,28 @@ def newmail(m):
         tm = TempMail()
         email = tm.get_email_address()
         r.set('email:{}:mail'.format(str(m.from_user.id)), email)
-        bot.send_message(m.chat.id, 'Your new Email: '+email)
+        bot.send_message(m.chat.id, '📧Your new Email: '+email)
 @bot.message_handler(commands=['mails'])
 def mails(m):
     try :
         #initialize Temp-Male and read recieved Mails.
         mail = r.get('email:{}:mail'.format(str(m.from_user.id)))
         if not mail:
-                bot.send_message(m.from_user.id, 'Make an email first.\nUse /newmail')
+                bot.send_message(m.from_user.id, '📧Make an email first.\nUse /newmail')
                 return
         parts = mail.split('@')
         tm = TempMail(login=parts[0], domain='@'+parts[1])
         mails = tm.get_mailbox()
         if not mails :
-                bot.send_message(m.from_user.id, 'There is no email...')
+                bot.send_message(m.from_user.id, '📭There is no email...')
         else:
             if 'error' in mails :
-                bot.send_message(m.from_user.id, 'There is no email...')
+                bot.send_message(m.from_user.id, '📭There is no email...')
             else:
                 print mails
                 for i in mails:
-                        bot.send_message(m.from_user.id, 'Mail from: '+i['mail_from']+'\n\nSubject: '+i['mail_subject']+'\n\nText: ' +i['mail_text'])
+                        bot.send_message(m.from_user.id, '📬Mail from: '+i['mail_from']+'\n\nSubject: '+i['mail_subject']+'\n\nText: ' +i['mail_text'])
     except:
-        bot.send_message(m.from_user.id, 'There is no email...')
+        bot.send_message(m.from_user.id, '📭There is no email...')
 
 bot.polling()
